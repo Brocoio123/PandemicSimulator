4563352eebc1720e1be3ea0768be1f5c5a27c033
Everardo Meireles <everardomeireles@gmail.com>
2020-09-14 20:24:34 +0200

    -cached hud images -both huds now display more information

--- canvas.js
+++ canvas.js
@@ -1,13 +1,17 @@
-let canvasPersons = [];
+var cachedImages = [new Image, new Image, new Image, new Image, new Image, new Image];
+var canvasPersons = [];
 
-let canvas = document.getElementById('mainCanvas');
-let canvasContext = canvas.getContext('2d');
+var canvas = document.getElementById('mainCanvas');
+var canvasContext = canvas.getContext('2d');
 
-let hud1 = document.getElementById('hud1');
-let hud1Context = hud1.getContext('2d');
+var hud1 = document.getElementById('hud1');
+var hud1Context = hud1.getContext('2d');
+
+var hud2 = document.getElementById('hud2');
+var hud2Context = hud2.getContext('2d');
 
 //redo this smarter
-let scaleFactor = 2;
+var scaleFactor = 2;
 
 //pseudo bounding boxes/colliders
 CellXSize = 24*scaleFactor;
@@ -19,13 +23,25 @@ canvas.width = 360*scaleFactor;
 hud1.height = 80 * scaleFactor;
 hud1.width = 160 * scaleFactor;
 hud1Context.font = "20px Arial";
+hud1Context.imageSmoothingEnabled = false;
 
+hud2.height = 280 * scaleFactor;
+hud2.width = 160 * scaleFactor;
+hud2Context.font = "20px Arial";
+
+cacheHudImages()
 spawnCanvasPersons();
 drawCanvasPerson();
 
 canvas.addEventListener('click', displayPersonInformation, false);
 setInterval(animateSpriteCanvasPersons, (300));
 setInterval(drawCanvasPersons, ((turnInterval/65)-((turnInterval/65)*0.25)));
+
+function cacheHudImages(){
+    for (var i = 0, len = cachedImages.length; i < len; i++) {
+        cachedImages[i].src = 'http://127.0.0.1/PandemicSimulator/sprites/chara'+ i +'Down0.png';
+    }
+}
 
 function drawCanvasPersons(){
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
@@ -59,29 +75,57 @@ function getPersonInformation(id){
             obj = {
                 id: person.id,
                 spriteId: person.spriteId,
-                name:person.name,
-                gender:person.name,
-                status:person.status
+                name: person.name,
+                status: person.status,
+                panicIndex: person.PI,
+                cautionIndex: person.CI,
+                infectionChance: person.IC
               };
         }
     });
     return(obj);
 }
 
+displayWorldInformation()
+function displayWorldInformation() {
+    hud2Context.clearRect(0, 0, hud2.width, hud2.height);
+    hud2Context.fillText("Global virus threat level: " + GVTL, (hud2.width*0.05), (hud2.height*0.1));
+    hud2Context.fillText("numberOfInfected: " + numberOfInfected, (hud2.width*0.05), (hud2.height*0.2));
+    hud2Context.fillText("numberOfHealthy " + numberOfHealthy, (hud2.width*0.05), (hud2.height*0.3));
+    //botar infected on the day before
+    hud2Context.fillText("time: " + hours%24 + ":00", (hud2.width*0.05), (hud2.height*0.4));
+    //only shows infected in the current screen
+    hud2Context.fillText("infections on screen: " + infectedInOneDay, (hud2.width*0.05), (hud2.height*0.5));
+    hud2Context.fillText("infected the previous day: " + infectedThePreviousDay, (hud2.width*0.05), (hud2.height*0.6));
+    return;
+}
+
 function displayPersonInformation(e) {
     mousePos = getMousePos(canvas, e);
     console.log(mousePos.x);
     console.log(mousePos.y);
+    let information = {};
+    let img = new Image;
     canvasPersons.forEach(canvasPerson => {
         if(mousePos.x > ((canvasPerson.y*CellYSize)-(canvasPerson.y*CellYSize/4)) && mousePos.x < ((canvasPerson.y*CellYSize)-(canvasPerson.y*CellYSize/4)+CellXSize)){
             if(mousePos.y > ((canvasPerson.x*CellXSize)+(canvasPerson.x*CellXSize/3)) && mousePos.y < (((canvasPerson.x*CellXSize)+(canvasPerson.x*CellXSize/3))+CellYSize)){
-                let information = {};
+                hud1Context.font = "20px Arial";
+                img.src = 'http://127.0.0.1/PandemicSimulator/sprites/chara'+ canvasPerson.spriteId +'Down0.png';
+                console.log(img.src)
                 information = getPersonInformation(canvasPerson.id);
                 hud1Context.clearRect(0, 0, hud1.width, hud1.height);
-                hud1Context.fillText("name: " + information.name, (hud1.width*0.1), (hud1.height*0.3));
-                hud1Context.fillText("gender: " + information.gender, (hud1.width*0.1), (hud1.height*0.6));
-                hud1Context.fillText("Status: " + information.status, (hud1.width*0.1), (hud1.height*0.9));
-                console.log("canvasPerson id is: " + canvasPerson.id);
+                hud1Context.fillText("name: " + information.name, (hud1.width*0.25), (hud1.height*0.18));
+                hud1Context.fillText("Status: " + information.status, (hud1.width*0.25), (hud1.height*0.36));
+                hud1Context.fillText("Panic: " + information.panicIndex.toFixed(2) + "%", (hud1.width*0.25), (hud1.height*0.54));
+                hud1Context.fillText("Caution: " + information.cautionIndex.toFixed(2) + "%", (hud1.width*0.25), (hud1.height*0.72));
+                hud1Context.fillText("Infection chance: " + information.infectionChance.toFixed(2) + "%", (hud1.width*0.25), (hud1.height*0.90));
+                hud1Context.drawImage(img, (hud1.width*0.05), (hud1.height*0.2), CellXSize,  CellYSize);
+                hud1Context.font = "10px Arial";
+                for (let i = 0, len = personsVar[canvasPerson.id].preventionMeasures.length; i < len; i++) {
+                    console.log(personsVar[canvasPerson.id].preventionMeasures[i])
+                    hud1Context.fillText(personsVar[canvasPerson.id].preventionMeasures[i], (hud1.width*0.01), ((hud1.height*0.7)+(i*10)));
+
+                }
                 return;
             }
         }
